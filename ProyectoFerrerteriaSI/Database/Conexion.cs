c1d7b72140e233680bb88cf274df5ae98002d4d9
robertocastillo <robertocastillo945@gmail.com>
2021-07-20 21:09:25 -0600

    agrege conexion  a db

---
+++ ProyectoFerrerteriaSI/Database/Conexion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFerrerteriaSI.Database
+{
+    public class Conexion
+    {
+
+        private string cadena => @"Server=tcp:su-mueble-server.database.windows.net,1433;Initial Catalog=FerreteriaDB;Persist Security Info=False;User ID=adminSuMueble;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+        public SqlConnection GetConexion => new SqlConnection(cadena); 
+
+    }
+}
