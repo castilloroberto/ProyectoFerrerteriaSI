71c9d52b5880e9bae798b8ecdad54536cf93dc54
Gabriela A. Gonzales <gonzalezespinoza1999@gmail.com>
2021-07-28 21:11:33 -0600

    Login

--- ProyectoFerrerteriaSI/Login.cs
+++ ProyectoFerrerteriaSI/Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoFerrerteriaSI.Database;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
 
 namespace ProyectoFerrerteriaSI
 {
@@ -17,9 +20,10 @@ namespace ProyectoFerrerteriaSI
             InitializeComponent();
         }
 
+        
         private void btn_entrar_Click(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
